b9c81f8802212db1193a9dd2b2d62a95dc4285ab
smandoki <80867148+smandoki@users.noreply.github.com>
2023-02-28 07:14:30 +0800

    Changed cart layout
    rendering cart items into a table instead of a cartItem component
    to keep columns aligned as widths change due to increasing
    quantity or price

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,29 +1,71 @@
 import styles from '../styles/Cart.module.css';
-import CartItem from './CartItem';
 
 function Cart({ products, cart, handleAddItem, handleRemoveItem }) {
+  const cartIsEmpty = Object.keys(cart).length === 0;
+
   return (
     <div className={styles.cart}>
-      {Object.keys(cart).length === 0 ? (
-        <h3>Cart is empty</h3>
-      ) : (
-        Object.keys(cart).map((key) => {
-          const product = products.find(
-            (product) => product.id === Number(key)
-          );
+      {cartIsEmpty && <h3>Cart is empty</h3>}
 
-          return (
-            <CartItem
-              key={product.id}
-              product={product}
-              quantity={cart[key]}
-              handleAddItem={handleAddItem}
-              handleRemoveItem={handleRemoveItem}
-            />
-          );
-        })
+      {/* Render cart items into table */}
+      {!cartIsEmpty && (
+        <table className={styles.table}>
+          <tbody>
+            <tr>
+              <th></th>
+              <th>Product</th>
+              <th>Price</th>
+              <th>Quantity</th>
+              <th>Subtotal</th>
+            </tr>
+            {Object.keys(cart).map((key) => {
+              const product = products.find(
+                (product) => product.id === Number(key)
+              );
+              return (
+                <tr key={product.id}>
+                  <td>
+                    <img
+                      className={styles.image}
+                      src={product.image}
+                      alt={product.title}
+                    />
+                  </td>
+                  <td>
+                    <h3 className={styles.title}>{product.title}</h3>
+                  </td>
+                  <td>
+                    <p>${product.price}</p>
+                  </td>
+                  <td>
+                    <span className={styles.quantity}>
+                      <button
+                        className={styles.button}
+                        onClick={() => handleRemoveItem(product.id)}
+                      >
+                        -
+                      </button>
+                      <p>{cart[key]}</p>
+                      <button
+                        className={styles.button}
+                        onClick={() => handleAddItem(product.id)}
+                      >
+                        +
+                      </button>
+                    </span>
+                  </td>
+                  <td>
+                    <p>${product.price * cart[key]}</p>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
       )}
-      {Object.keys(cart).length > 0 ? (
+
+      {/* Render cart total price */}
+      {!cartIsEmpty && (
         <h3 className={styles.total}>
           Total: $
           {Object.keys(cart).reduce((total, key) => {
@@ -33,7 +75,7 @@ function Cart({ products, cart, handleAddItem, handleRemoveItem }) {
             return total + cart[key] * product.price;
           }, 0)}
         </h3>
-      ) : null}
+      )}
     </div>
   );
 }
